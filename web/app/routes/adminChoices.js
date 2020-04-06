db6020ceaf41e88a5c97b5b711370ea527d6da85
Marjana Cowan <mdcowan@student.fullsail.edu>
2020-04-06 09:40:21 -0400

    choices route update

--- web/app/routes/adminChoices.js
+++ web/app/routes/adminChoices.js
@@ -1,32 +1,27 @@
 // import the express router
 const router = require('express').Router();
 // load the controller
-const questionCtrl = require('../controllers/questions');
+const choiceCtrl = require('../controllers/choices');
 const validationCtrl = require('../controllers/validation');
-// GET /admin/questions/new - loads the form to create a new decision
-router.get('/new', questionCtrl.renderQuestionForm);
-// POST /admin/questions/new - validate the data and than save it
+// GET /admin/choices/new - loads the form to create a new choice
+router.get('/new', choiceCtrl.renderChoicesForm);
+// POST /admin/choices/new - validate the data and than save it
 router.post('/new', [
-  validationCtrl.validate('createQuestions'),
-  questionCtrl.renderQuestionFormWithErrors,
-  questionCtrl.saveQuestion,
+  validationCtrl.validate('createChoices'),
+  choiceCtrl.renderChoicesFormWithErrors,
+  choiceCtrl.saveChoices,
 ]);
-// GET /admin/questions/edit/:id - loads the edit form
-router.get('/edit/:id', questionCtrl.renderEditForm);
-// POST /admin/questions/edit/:id - validate the data and than save it
+// GET /admin/choices/edit/:id - loads the edit form
+router.get('/edit/:id', choiceCtrl.renderEditForm);
+// POST /admin/choices/edit/:id - validate the data and than save it
 router.post('/edit/:id', [
-  validationCtrl.validate('editQuestions'),
-  questionCtrl.renderQuestionFormWithErrors,
-  questionCtrl.saveQuestion,
+  validationCtrl.validate('editChoices'),
+  choiceCtrl.renderChoicesFormWithErrors,
+  choiceCtrl.saveChoices,
 ]);
-// GET /admin/questions/delete/:id - deletes a question
+// GET /admin/choices/delete/:id - deletes a Choices
 router.get('/delete/:id', [
-  validationCtrl.validate('deleteQuestion'),
-  questionCtrl.goBackOnError,
-  questionCtrl.deleteQuestion,
+  validationCtrl.validate('deleteChoices'),
+  choiceCtrl.goBackOnError,
+  choiceCtrl.deleteChoices,
 ]);
-
-// GET /admin/questions/:id - loads the detail page
-router.get('/:id', questionCtrl.renderAdminQuestionDetail);
-// export the route from this file
-module.exports = router;
